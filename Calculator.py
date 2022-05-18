fdf1bd8be852325a130258d13060188b230e3704
lullebulle2 <lvmd04@gmail.com>
2022-05-18 16:32:33 +0200

    rätta linjens ekvation, tagit bort klassen och mainmain.

--- Calculator.py
+++ Calculator.py
@@ -1,93 +1,28 @@
 # Imports
-
-
-
-import math          # behöver för att kuna använd pi korrekt samt eulers numer (e)
-import time  #time.sleep
+import math  # Behöver för att kuna använd pi korrekt samt eulers numer (e)
+import time  # Time.sleep
 import os
-
-# clearconsol.
-
 # Global variables
-
-# Classes
-class Name:
-    def __init__(self, name) -> None:
-        self.name = name
-
 
 # Functions
 def cs():
     """
-    denna funktion gör så att det ser lite bättre ut och att allt inte ska komma på en gång, gjorde funktionen för att det skulle gå lite långsammare
+    Denna funktion gör så att det ser lite bättre ut och att allt inte ska komma på en gång, gjorde funktionen för att det skulle gå lite långsammare.
     Inputen är 'time.sleep' vilket gör så att programmet väntar lite innan nästa funktion körs igång och
     'os.system' vilket gör så att consolen blir ren och det gör det lättare att se vad som händer.
     """
     time.sleep(2)
     os.system('Clear')
 
-def name():
-    name = input("write your name and klick ENTER to start. ")
-    user = Name(name)
-    main(user)
-
-def mainmain():
+def main():   
     """
-    här är sidomenyn, jag har 2 som är ungefär lika dana eftersom jag inte vet hur man kan ta bort en sak ur en funktion efter den 
-    har använts en gång. gjorde 2 olika metoder som gör samma bara att hen här inte har 'welcome' och den har 'press '#' if you want to quit'.
-    den här funktionen tillåter dig enbart att välja hur du vill calkulera.
+    Menyn, tillåter dig att välja vad du vill calculera. Mellan varje if respektive elif i denna funktion kallar jag på en 
+    annan funktion som innerhåller matematiken nödvändig för att calculera det du har tryckt in. exempelvis: Addition: 
+    efter första if satsen har jag en funktion som heter Addition där jag har kodan så att man kan lägga in 2 numer och sedan får man ut summan.
     """
+    print("\n\n\n\n\n")
     while True:
-        print("""how would you like to calculate?
-            [1] Addition
-            [2] Subbtrakton
-            [3] Multiplikation
-            [4] Divitions
-            [5] percantage
-            [6] Square
-            [7] e^x
-            [8] pi
-            [#] press '#' if you want to quit.
-            """)
-        number = input("Chose a number: ")
-        if "1" == number:
-            addition()
-            cs()
-        elif "2" == number:
-            subbtraktion()
-            cs()
-        elif "3" == number:
-            multiplikation()
-            cs()
-        elif "4" == number:
-            divition()
-            cs()
-        elif "5" == number:
-            percentage()
-            cs()
-        elif "6" == number:
-            square()
-            cs()
-        elif "7" == number:
-            e()
-            cs()
-        elif "8" == number:
-            pi()
-            cs()
-            break
-        elif "#" == number:
-            visit_saveeverything()
-        else:
-            print("sir/ma'ma, please insurt a number")
-
-
-def main(user):   
-    """
-    först menyn, tillåter dig att välja vad du vill calculera.
-    """
-    print(f"\n\nwelcome {user.name}\n")
-    while True:
-        print("""how would you like to calculate?
+        print("""How would you like to calculate?
             [1] Addition                                                    
             [2] Subbtrakton
             [3] Multiplikation
@@ -96,6 +31,8 @@ def main(user):
             [6] Square
             [7] e^x
             [8] pi
+            [9] y=kx+m
+            [#] Write '#' to quit or se history
             """)
         number = input("Chose a number: ")
         if "1" == number:
@@ -122,34 +59,44 @@ def main(user):
         elif "8" == number:
             pi()
             cs()
+        elif "9" == number:
+            ykxm()
+            cs()
+        elif "#" == number:
+            visit_saveeverything()
+            cs()
             break
         else:
             print("sir/ma'ma, please insurt a number")
 
+    """
+    Här är första tället i koden där raden "with open("saveeverything.txt", "a", encoding="utf-8") as sav:".
+    Jag har med den i varje funktion då jag vill spara svaret man får och så att man akn gå tillbaka och se vad man har gjort.
+    """
 def addition():
-    with open("saveeverything.txt", "a", encoding="utf-8") as sav:
-        number_choice = int(input("insurt a number: "))
-        number_yes = int(input("insurt a nother number: "))
+    with open("saveeverything.txt", "a", encoding="utf-8") as sav:    
+        number_choice = int(input("insert a number: "))
+        number_yes = int(input("insert a number: "))
         number_addition = number_choice + number_yes
         number_combine = (f"{number_choice} + {number_yes} = {number_addition}")
         print(number_combine)
         sav.write(number_combine + "\n")    
         sav.close()
         cs()
-        mainmain()
+        main()
 
 
 def subbtraktion():
     with open("saveeverything.txt", "a", encoding="utf-8") as sav:
-        number_Choice = int(input("insurt a number: "))
+        number_choice = int(input("insurt a number: "))
         number_yes = int(input("insurt a nother number: "))
-        number_subbtraktion = number_Choice - number_yes
-        number_combine = (f"{number_Choice} - {number_yes} = {number_subbtraktion}")
+        number_subbtraktion = number_choice - number_yes
+        number_combine = (f"{number_choice} - {number_yes} = {number_subbtraktion}")
         print(number_combine)
         sav.write(number_combine + "\n")
         sav.close()
         cs()
-        mainmain()
+        main()
 
 
 def multiplikation():
@@ -162,23 +109,19 @@ def multiplikation():
         sav.write(number_combine + "\n")
         sav.close()
         cs()
-        mainmain()
+        main()
 
 def divition():
     with open("saveeverything.txt", "a", encoding="utf-8") as sav:
         number_choice = int(input("insert a number: "))
         number_yes = int(input("insert a nother number: "))
         number_divition = number_choice / number_yes
-        number_divition1 = number_choice // number_yes     #rundar av exempelvis om det blir 0,6 eller 0,9 så rundas det ned till 0.
         number_combine = (f"{number_choice} / {number_yes} = {number_divition}")
-        number_combine1 = (f"Rounded down: {number_choice} / {number_yes} = {(number_divition1):.2f}") #":.2f" genom att skriva detta så avrundar datorn talet till 2 decimaler.
         print(number_combine)
-        print(number_combine1)
         sav.write(number_combine + "\n")
-        sav.write(number_combine1 + "\n")
         sav.close()
         cs()
-        mainmain()
+        main()
 
 
 
@@ -187,7 +130,7 @@ def percentage():
         print("how much is x percent of y")
         x = int(input("insurt x: "))
         y = int(input("insurt y: "))
-        xy = x / y
+        xy = x / y                         # ekv för att räkna ut procent.
         times=xy
         time = times * 100
         percent = (f"{x} is {time:.3f}% out of {y}")
@@ -195,7 +138,7 @@ def percentage():
         sav.write(percent + "\n")
         sav.close()
         cs()
-        mainmain()
+        main()
 
 
 def square():
@@ -204,13 +147,13 @@ def square():
     """
     number_square = int(input("insurt a number to Square root OBS x > 0: "))
     print(math.sqrt(number_square))
-    mainmain()
+    main()
 
 def e():
     """
     här har jag använt mig av math.e vilket ger ett par desimaler från konstanten e.
+    Den här funktionen räknar ut e upphöjt till det tal du har skrivit in.
     """
-
     with open("saveeverything.txt", "a", encoding="utf-8") as sav:
         print("write what you want in x (e^x): ")
         number_x = int(input(" "))
@@ -220,12 +163,14 @@ def e():
         sav.write(oliver + "\n")
         sav.close()
         cs()
-        mainmain()
+        main()
 
 
-def pi(user):
+def pi():
     """
     jag importa math eftersom jag ville använda math.pi vilket ger hela eller många desimaler av pi.
+    På varje ställe där man ska skriva in sitt numer skrev jag en variable i alfabetisk årdning, det gjorde det änklare för mig när jag koda.
+    Samma sak med namnen, det vart lättare för mig när jag koda.
     """
     print("""
     [1] pi^
@@ -246,7 +191,7 @@ def pi(user):
             lukas = (f"pi^{a} = {py1}")
             print(f"pi^{a} = {py1}")
             sav.write(lukas + "\n")
-            mainmain(user)
+            main()
 
         elif "2" in number_x:
             print("x^pi, write your x: ")
@@ -255,7 +200,7 @@ def pi(user):
             albion = (f"{b}^pi = {py2}")
             print(f"{b}^pi = {py2}")
             sav.write(albion + "\n")
-            mainmain(user)
+            main()
         
         elif "3" in number_x:
             print("x/pi, write your x: ")
@@ -265,7 +210,7 @@ def pi(user):
             zion = (f"{c}/pi = {py3} ~ {py35}")
             print(f"{c}/pi = {py3} ~ {py35}")
             sav.write(zion + "\n")
-            mainmain(user)
+            main()
 
         elif "4" in number_x:
             print("pi/x, write your x: ")
@@ -275,7 +220,7 @@ def pi(user):
             alexis = (f"pi/{d} = {py4} ~ {py45}")
             print(f"pi/{d} = {py4} ~ {py45}")
             sav.write(alexis + "\n")
-            mainmain(user)
+            main()
 
         elif "5" in number_x:
             print("x*pi, write your x ")
@@ -284,7 +229,7 @@ def pi(user):
             victor = (f"{e}*pi = {py5}")
             print(f"{e}*pi = {py5}")
             sav.write(victor + "\n")
-            mainmain(user)
+            main()
 
         elif "6" in number_x:
             print("x+pi, write your x ")
@@ -293,7 +238,7 @@ def pi(user):
             jakob = (f"{f} + pi = {py6}") 
             print(f"{f} + pi = {py6}")
             sav.write(jakob + "\n")
-            mainmain(user)
+            main()
 
         elif "7" in number_x:
             print("x-pi, write your x: ")
@@ -302,7 +247,7 @@ def pi(user):
             max = (f"{g} - pi = {py7}")
             print(f"{g} - pi = {py7}")
             sav.write(max + "\n")
-            mainmain(user)
+            main()
 
         elif "8" in number_x:
             print("pi-x, write your x: ")
@@ -311,36 +256,58 @@ def pi(user):
             tim = (f"pi - {h} = {py8}")
             print(f"pi - {h} = {py8}")
             sav.write(tim + "\n")
-            mainmain(user)
-        
+            main()
         else:
-            mainmain(user)
+            main()
     sav.close()
+
+def ykxm():
+    """
+    Den funktionen räknar ut den rätta linjerns ekvation med hjälp av de kordinater som du matar in.
+    """
+    with open("saveeverything.txt", "a", encoding="utf-8") as sav:
+        print("""
+        k= (y2-y1)/(x2-x1)
+        """)
+        y2 = int(input("put in your y2: "))
+        y1 = int(input("put in your y1: "))
+        x2 = int(input("put in your x2: "))
+        x1 = int(input("put in your x1: "))
+        ytot = y2 - y1
+        xtot = x2 - x1
+        k = ytot/xtot
+        print(f"your k is {k} l.e")
+        m1 = int(input("put in your x cordinat: "))
+        m2 = int(input("put in your y cordinat:"))
+        km1 = k*m1                          # Tog k från tidigare i funktionen.
+        m = m2/km1
+        print(f"{m}")
+        print(f"your linjer ekv: y={k}x+{m}")
+        spara = (f"your linjer ekv: y={k}x+{m}")
+        sav.write(spara + "\n")
+        sav.close()
+        main() 
 
 def visit_saveeverything():
     """
     Den här funktionen är enbart så man kan gå tillbaka för att kolla vilka uträkningar man har gjort tidigare.
     Jag frågar först om användaren vill avsluta eller inte, om de vill kommer man hitt och sedan kan man välja om man vill avluta eller ej.
-    om man inte vill kommer man tillbaka till 'huvudsidan' """
+    om man inte vill kommer man tillbaka till 'huvudsidan'.
+    desutom 
+    """
     with open("saveeverything.txt", "r", encoding="utf8") as sav:
         x = input("do you want to se what you have been calculated? (y/n)")
         if "y" in x:
             for s in sav.readlines():
                 print(s)
             input(" ")
-        
         else:
             y = input("whould you like to quit or continu? (q/c)")
             if "c" in y:
-                mainmain()  #huvudsidan
+                main() 
             else:
-                quit()
-# fil som sparar allt man har räknat ut. och som kan hämta tillbak datan.
-#
-# Main-caller
-
+                os._exit()
 
 if __name__ == "__main__":
-    name()
-    
+    main()  
     
